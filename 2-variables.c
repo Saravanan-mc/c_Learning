75a13ef9af870b8e37aca23e74e2d74fff6ceb32
SARAVANAN M <158152535+Saravanan-mc@users.noreply.github.com>
2025-09-12 17:19:48 +0530

    Create 2-variables.c

---
+++ 2-variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+  int a=2;
+  float b = 1.234;
+  char c = 'a';
+  double d=1.12435465676;
+  
+  printf("%d \n",a);
+  printf("%f \n",b);
+  printf("%c \n",c);
+  printf("%lf \n",d);
+}
