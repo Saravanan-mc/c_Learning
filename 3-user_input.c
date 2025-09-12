6683c8a72cf1bc95c5ef56764e96b7112f113a90
SARAVANAN M <158152535+Saravanan-mc@users.noreply.github.com>
2025-09-12 17:32:39 +0530

    Create 3-user_input.c

---
+++ 3-user_input.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    float f;
+    char c;
+    double d;
+    scanf("%d",&n);
+    scanf("%f",&f);
+    scanf("%c",&c);
+    scanf("%lf",&d);
+    printf("%d  \n %f \n %c \n %lf",n,f,c,d);
+}
