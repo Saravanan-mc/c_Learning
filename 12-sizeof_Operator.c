d63f0f775f18a274b1e42ffc0c75b896f4151632
SARAVANAN M <158152535+Saravanan-mc@users.noreply.github.com>
2025-09-15 18:51:45 +0530

    Create 12-sizeof_Operator.c

---
+++ 12-sizeof_Operator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a;
+    double b;
+    char c;
+
+    printf("Size of int = %zu\n", sizeof(a));
+    printf("Size of double = %zu\n", sizeof(b));
+    printf("Size of char = %zu\n", sizeof(c));
+
+    return 0;
+}
